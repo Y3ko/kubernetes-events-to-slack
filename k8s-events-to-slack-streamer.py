e23b7a320e9d62a2eac9aacfabda54a38759b5f9
Y3ko <31895562+Y3ko@users.noreply.github.com>
2024-07-31 15:09:53 +0300

    new version update kubernetes package ver=v30.1.0

--- k8s-events-to-slack-streamer.py
+++ k8s-events-to-slack-streamer.py
@@ -5,187 +5,229 @@ import sys
 import time
 import json
 import logging
-import http.client
-import kubernetes
 import traceback
+import requests
+from kubernetes import client, config, watch
+from kubernetes.client import CustomObjectsApi
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
 def read_env_variable_or_die(env_var_name):
     value = os.environ.get(env_var_name, '')
-    if value == '':
-        message = f'Env variable {env_var_name} is not defined or set to empty string. '
-        message += 'Set it to non-empty string and try again'
+    if not value:
+        message = f'{env_var_name} isimli ortam değişkeni tanımlanmamış veya boş bir değer olarak ayarlanmış. '
+        message += 'Lütfen bu değeri dolu bir string ile ayarlayıp tekrar deneyin.'
         logger.error(message)
         raise EnvironmentError(message)
     return value
 
-
-# Slack web hook example
-# https://hooks.slack.com/services/XXXXXXX/XXXXXXX/XXXXXXXXXXXX
 def post_slack_message(hook_url, message):
-    logger.info(f'Posting the following message:\n{message}')
-    headers = {'Content-type': 'application/json'}
-    connection = http.client.HTTPSConnection('hooks.slack.com')
-    connection.request('POST',
-                       hook_url.replace('https://hooks.slack.com', ''),
-                       message,
-                       headers)
-    response = connection.getresponse()
-    print(response.read().decode())
+    logger.info(f'Aşağıdaki mesaj gönderiliyor:\n{message}')
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(hook_url, headers=headers, data=message)
+    if response.status_code != 200:
+        logger.error(f"Slack'e mesaj gönderilemedi: {response.status_code}, {response.text}")
+    else:
+        logger.info(f"Mesaj başarıyla Slack'e gönderildi.")
 
-
-# Return even reason in upper case to keep it consistent
-# no matter how API changes
 def get_event_reason(event):
-    return event['object'].reason.upper()
-
+    """ CoreV1Event nesnesinin doğrudan erişimi """
+    return event.reason.upper() if event.reason else 'UNKNOWN'
 
 def format_k8s_event_to_slack_message(event_object, notify=''):
     event = event_object['object']
-
-    def timestamp(obj):
-        return obj.strftime('%d/%m/%Y %H:%M:%S %Z') if obj else "no info"
-
     message = {
         'attachments': [{
-            'color': '#36a64f',
-            'title': event.message,
-            'text': 'event type: {}, event reason: {}'.format(event_object['type'], event.reason),
-            'footer': 'First time seen: {}, Last time seen: {}, Count: {}'.format(timestamp(event.first_timestamp),
-                                                                                  timestamp(event.last_timestamp),
-                                                                                  event.count),
+            'color': '#36a64f',  # Yeşil (Normal olaylar)
+            'title': event.message if event.message else 'Mesaj yok',
+            'text': f"Etkinlik türü: {event_object['type'] if event_object['type'] else 'Bilinmiyor'}, "
+                    f"Etkinlik nedeni: {event.reason if event.reason else 'Bilinmiyor'}",
+            'footer': f"İlk görülme: {event.first_timestamp if event.first_timestamp else 'Bilgi yok'}, "
+                      f"Son görülme: {event.last_timestamp if event.last_timestamp else 'Bilgi yok'}, "
+                      f"Sayım: {event.count if event.count else 'Bilgi yok'}",
             'fields': [
                 {
-                    'title': 'Involved object',
-                    'value': 'kind: {}, name: {}, namespace: {}'.format(event.involved_object.kind,
-                                                                        event.involved_object.name,
-                                                                        event.involved_object.namespace),
+                    'title': 'İlgili nesne',
+                    'value': f"Tür: {event.involved_object.kind if event.involved_object.kind else 'Bilinmiyor'}, "
+                             f"Ad: {event.involved_object.name if event.involved_object.name else 'Bilinmiyor'}, "
+                             f"Namespace: {event.involved_object.namespace if event.involved_object.namespace else 'Bilinmiyor'}",
                     'short': 'true'
                 },
                 {
                     'title': 'Metadata',
-                    'value': 'name: {}, creation time: {}'.format(event.metadata.name,
-                                                                  timestamp(event.metadata.creation_timestamp)),
+                    'value': f"Ad: {event.metadata.name if event.metadata.name else 'Bilinmiyor'}, "
+                             f"Oluşturma zamanı: {event.metadata.creation_timestamp if event.metadata.creation_timestamp else 'Bilgi yok'}",
                     'short': 'true'
                 }
             ],
         }]
     }
-    if event.type == 'Warning':
-        message['attachments'][0]['color'] = '#cc4d26'
-        if notify != '':
-            message['text'] = f'{notify} there is a warning for you to check'
+
+    event_reason = get_event_reason(event)
+
+    if event_reason in ['KILLING', 'FAILED', 'BACKOFF', 'UNHEALTHY', 'CREATEFAILED', 'DELETED', 'TOPCONNECTIONFAILED', 'NODEHASNODISKPRESSURE']:
+        message['attachments'][0]['color'] = '#cc0000'  # Kırmızı (Kritik hatalar)
+    elif event.type.upper() in ['PULLING', 'SCALINGREPLICASET', 'SCHEDULED', 'NOTRECONCILINGDRAIN', 'EVICTIONTHRESHOLDMET']:
+        message['attachments'][0]['color'] = '#ffcc00'  # Sarı (Orta düzeydeki hatalar)
+        if notify:
+            message['text'] = f'{notify}, kontrol etmeniz gereken bir uyarı var'
+    elif event.type.upper() == 'WARNING':
+        message['attachments'][0]['color'] = '#ffcc00'  # Sarı (Orta düzeydeki hatalar)
+        if notify:
+            message['text'] = f'{notify}, kontrol etmeniz gereken bir uyarı var'
 
     return json.dumps(message)
-
 
 def format_error_to_slack_message(error_message):
     message = {
         'attachments': [{
             'color': '#8963B9',
-            'title': 'Ooopsy oopsy!',
-            'text': f'Check logs! Failed to process events with error: {error_message}'
+            'title': 'Hata Bildirimi',
+            'text': f'Logları kontrol edin! Hata ile olaylar işlenemedi: {error_message}'
         }]
     }
     return json.dumps(message)
 
-
-def stream_events(kubernetes, k8s_namespace_name, timeout):
-    v1 = kubernetes.client.CoreV1Api()
-    k8s_watch = kubernetes.watch.Watch()
+def stream_events(k8s_client, k8s_namespace_name, timeout):
+    v1 = k8s_client.CoreV1Api()
+    k8s_watch = watch.Watch()
     if k8s_namespace_name:
-        return k8s_watch.stream(v1.v1.list_namespaced_event, k8s_namespace_name, timeout_seconds=timeout)
+        return k8s_watch.stream(v1.list_namespaced_event, namespace=k8s_namespace_name, timeout_seconds=timeout)
     else:
         return k8s_watch.stream(v1.list_event_for_all_namespaces, timeout_seconds=timeout)
 
+def handle_event(event, reasons_to_skip, users_to_notify, slack_web_hook_url, cached_event_uids):
+    try:
+        logger.debug(str(event))
+        event_obj = event['object']
+        event_reason = get_event_reason(event_obj)
+        event_uid = event_obj.metadata.uid
+        if event_reason in reasons_to_skip:
+            logger.info(f'Etkinlik nedeni {event_reason} ve atlanacak nedenler listesinde. Atlanıyor.')
+            return
+        if event_uid in cached_event_uids:
+            logger.info(f'Etkinlik kimliği {event_uid} ve önbellekteki etkinlikler listesinde. Atlanıyor.')
+            return
+        message = format_k8s_event_to_slack_message(event, users_to_notify)
+        post_slack_message(slack_web_hook_url, message)
+        cached_event_uids.append(event_uid)
+    except Exception as error:
+        logger.exception(error)
+        stack_trace = traceback.format_exc()
+        message = f'Olayı işleyemedik, hata:\n{stack_trace}\n{event}'
+        post_slack_message(slack_web_hook_url, format_error_to_slack_message(message))
+        time.sleep(30)
+
+def send_pod_list_to_slack(k8s_client, k8s_namespace_name, slack_web_hook_url, stable_system_message_sent):
+    v1 = k8s_client.CoreV1Api()
+    try:
+        if k8s_namespace_name:
+            pods = v1.list_namespaced_pod(namespace=k8s_namespace_name)
+        else:
+            pods = v1.list_pod_for_all_namespaces()
+
+        pod_list = []
+        all_pods_running = True
+        for pod in pods.items:
+            # Pod phase kontrolü
+            if pod.status.phase != "Running":
+                pod_list.append(f"Pod Adı: {pod.metadata.name}, Namespace: {pod.metadata.namespace}, Durum: {pod.status.phase}")
+                all_pods_running = False
+
+            # Container status kontrolü
+            if pod.status.container_statuses:
+                for container_status in pod.status.container_statuses:
+                    if container_status.state.waiting and container_status.state.waiting.reason in ["CrashLoopBackOff", "OOMKilled"]:
+                        pod_list.append(f"Pod Adı: {pod.metadata.name}, Namespace: {pod.metadata.namespace}, Durum: {container_status.state.waiting.reason}")
+                        all_pods_running = False
+
+        if all_pods_running and not stable_system_message_sent:
+            message = {
+                'attachments': [{
+                    'color': '#36a64f',  # Yeşil (Stabil sistem)
+                    'title': 'Sistem Stabil',
+                    'text': 'Tüm podlar "Running" durumunda.',
+                    'footer': 'Pod Listesi',
+                }]
+            }
+            post_slack_message(slack_web_hook_url, json.dumps(message))
+            stable_system_message_sent = True
+        elif not all_pods_running:
+            stable_system_message_sent = False
+            message = {
+                'attachments': [{
+                    'color': '#cc0000',  # Kırmızı (Uyarı olayları)
+                    'title': 'RUNNING DURUMUNDA OLMAYAN POD LISTESI',
+                    'text': '\n'.join(pod_list),
+                    'footer': 'Pod Listesi',
+                }]
+            }
+            post_slack_message(slack_web_hook_url, json.dumps(message))
+    except Exception as e:
+        logger.error(f"Pod listesini Slack'e gönderirken hata oluştu: {e}")
+        stack_trace = traceback.format_exc()
+        message = f'Pod listesini Slack\'e gönderirken hata oluştu:\n{stack_trace}'
+        post_slack_message(slack_web_hook_url, format_error_to_slack_message(message))
+    return stable_system_message_sent
 
 def main():
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG if os.environ.get('K8S_EVENTS_STREAMER_DEBUG', False) else logging.INFO,
+                        format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-    if os.environ.get('K8S_EVENTS_STREAMER_DEBUG', False):
-        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
-                            format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    else:
-        logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-                            format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-
-    logger.info('Reading configuration...')
+    logger.info('Konfigürasyon okunuyor...')
     k8s_namespace_name = os.environ.get('K8S_EVENTS_STREAMER_NAMESPACE', '')
-    # Uppercase event reasons to skip so we can consistently compare no matter how exactly
-    # user enters them
     reasons_to_skip = os.environ.get('K8S_EVENTS_STREAMER_LIST_OF_REASONS_TO_SKIP', '').upper().split()
-    skip_delete_events = os.environ.get('K8S_EVENTS_STREAMER_SKIP_DELETE_EVENTS', False)
-    if skip_delete_events is not False:
-        logger.info(f'K8S_EVENTS_STREAMER_SKIP_DELETE_EVENTS is set to {skip_delete_events}')
-        logger.info('Added SUCCESSFULDELETE to the list of reasons to skip event')
+    skip_delete_events = os.environ.get('K8S_EVENTS_STREAMER_SKIP_DELETE_EVENTS', '').lower() == 'true'
+    last_pod_check_time = time.time()
+    if skip_delete_events:
+        logger.info('SUCCESSFULDELETE, atlanacak nedenler listesine eklendi')
         reasons_to_skip.append('SUCCESSFULDELETE')
     users_to_notify = os.environ.get('K8S_EVENTS_STREAMER_USERS_TO_NOTIFY', '')
     slack_web_hook_url = read_env_variable_or_die('K8S_EVENTS_STREAMER_INCOMING_WEB_HOOK_URL')
-    logger.info('Configuration is OK')
-    logger.info('Running with the following parameters')
+    pod_check_interval = 10  # 10 saniye olarak ayarlandı
+
+    logger.info('Konfigürasyon tamam')
+    logger.info('Aşağıdaki parametrelerle çalışıyor')
     logger.info(f'K8S_EVENTS_STREAMER_NAMESPACE: {k8s_namespace_name}')
     logger.info(f'K8S_EVENTS_STREAMER_LIST_OF_REASONS_TO_SKIP: {reasons_to_skip}')
     logger.info(f'K8S_EVENTS_STREAMER_USERS_TO_NOTIFY: {users_to_notify}')
     logger.info(f'K8S_EVENTS_STREAMER_INCOMING_WEB_HOOK_URL: {slack_web_hook_url}')
+    logger.info(f'K8S_EVENTS_STREAMER_POD_CHECK_INTERVAL: {pod_check_interval} saniye')
 
-    logger.info('Loading k8s config...')
-    kubernetes.config.load_incluster_config()
-    # This one is for local testing
-    # kubernetes.config.load_kube_config()
+    logger.info('K8s yapılandırması yükleniyor...')
+    config.load_incluster_config()
+
+    # Mevcut pod listesini Slack'e gönder
+    stable_system_message_sent = False
+    stable_system_message_sent = send_pod_list_to_slack(client, k8s_namespace_name, slack_web_hook_url, stable_system_message_sent)
 
     cached_event_uids = []
     while True:
         try:
-            logger.info('Processing events for two hours...')
-            for event in stream_events(kubernetes, k8s_namespace_name, 7200):
-                try:
-                    logger.debug(str(event))
-                    event_reason = get_event_reason(event)
-                    event_uid = event['object'].metadata.uid
-                    if event_reason in reasons_to_skip:
-                        logger.info(f'Event reason is {event_reason} and it is in the skip list. So skip it')
-                        continue
-                    if event_uid in cached_event_uids:
-                        logger.info(f'Event id is {event_uid} and it is in the cached events list. So skip it')
-                        continue
-                    message = format_k8s_event_to_slack_message(event, users_to_notify)
-                    post_slack_message(slack_web_hook_url, message)
-                    cached_event_uids.append(event_uid)
-                # deal with parsing errors
-                except Exception as error:
-                    logger.exception(error)
-                    logger.error(event)
-                    stack_trace = traceback.format_exc()
-                    message = f'Failed to parse event and error is:\n{stack_trace}\n{event}'
-                    post_slack_message(
-                        slack_web_hook_url,
-                        format_error_to_slack_message(message)
-                    )
-                    time.sleep(30)
-                    continue
+            logger.info('İki saat boyunca olaylar işleniyor...')
+            for event in stream_events(client, k8s_namespace_name, 7200):
+                handle_event(event, reasons_to_skip, users_to_notify, slack_web_hook_url, cached_event_uids)
+
+                # Check if 5 seconds have passed since the last pod check
+                if time.time() - last_pod_check_time >= pod_check_interval:
+                    stable_system_message_sent = send_pod_list_to_slack(client, k8s_namespace_name, slack_web_hook_url, stable_system_message_sent)
+                    last_pod_check_time = time.time()
+
         except TimeoutError as timeout_error:
             logger.exception(timeout_error)
-            logger.warning('Wait 30 sec and check again due time out error.')
+            logger.warning('Zaman aşımı hatası nedeniyle 30 saniye bekleyin ve tekrar kontrol edin.')
             time.sleep(30)
-            continue
-        # Deal with unexpected stuff
         except Exception as error:
             logger.exception(error)
             stack_trace = traceback.format_exc()
-            message = f'Unexpected error:\n{stack_trace}'
+            message = f'Beklenmeyen hata:\n{stack_trace}'
             post_slack_message(slack_web_hook_url, format_error_to_slack_message(message))
             time.sleep(30)
-            continue
 
-        # Clean cached events after 2 hours, default event ttl is 1 hour in K8s
-        # --event-ttl duration     Default: 1h0m0s
-        # https://kubernetes.io/docs/reference/command-line-tools-reference/kube-apiserver/
         cached_event_uids = []
 
-    logger.info('Done')
-
+    logger.info('Tamamlandı')
 
 if __name__ == '__main__':
     main()
